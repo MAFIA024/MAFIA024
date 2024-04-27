966a82662e3480bd6ec20c0658a821e89dc170b9
MAFIA024 <168256756+MAFIA024@users.noreply.github.com>
2024-04-27 12:06:44 +0530

    python code
    how to use if/else function

---
+++ main (1).py
@@ -0,0 +1,11 @@
+# if/else
+montheley = int(input("Enter your monthely net salary: "))
+print(montheley)
+
+if(montheley<10000):
+  print("You are not eligible for lone")
+else:
+  print(montheley>10000)
+  print("You are eligible for lone")
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# if/else
+a = int(input("Enter a number: "))
+print(a)
+
+if(a>350):
+  print("you are selected")
+else:
+  print(a<200)
+  print("you canot selectes")
+
+
