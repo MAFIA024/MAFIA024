9acadab0001dcaca6a2730d37c355287de7f03be
MAFIA024 <168256756+MAFIA024@users.noreply.github.com>
2024-04-27 12:55:04 +0530

    python
    practices file

---
+++ mafia (2).py
@@ -0,0 +1,10 @@
+# if/else "\n" new line 
+h = int(input("Enter Your Birth year:"))
+print(h)
+if(h>2000):
+  print("\nYou are joine our football team")
+  print("\nCongratulation")
+else:
+   print(h<1999)
+   print("\nYou not elegeble to join our football team")
+   print("\nsorry")
