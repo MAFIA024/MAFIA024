6cdec20f98410854ac42c360dab56f634e585342
MAFIA024 <168256756+MAFIA024@users.noreply.github.com>
2024-04-27 13:24:19 +0530

    Add files via upload
    practices

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# if/else
-a = int(input("Enter a number: "))
-print(a)
-
-if(a>350):
-  print("you are selected")
+u = int(input("Enter Your %:"))
+print(u)
+if(u>60):
+  print("\n You are able to joine our NIT College")
+  print("\n Congratulations")
+  print("\n Upload your Documents")
 else:
-  print(a<200)
-  print("you canot selectes")
-
+  print(u<59)
+  print("\n You are not able to join our NIT College")
+  print("\n Try Again Next Year")
+  print("\n Sorry")
+  
+  if(u<59):
+    print("\n You cane Joine our Classes to imporve your %")
+  
+  else:
+    print(u<59)
+    print("\n Upload your Documents")
 
